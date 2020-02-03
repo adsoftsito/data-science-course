e00a33e40c93a1721454da9e06cb817fb203b28b
adsoftsito <adsoft@live.com.mx>
2020-02-03 13:26:19 -0600

    add controllers censos, denues

--- day1/test-data-rest-api/api/controllers/data.controller.js
+++ day1/test-data-rest-api/api/controllers/data.controller.js
@@ -1,5 +1,8 @@
 const { Estados } = require('../models'); // Sequelize
 const { Municipios } = require('../models'); // Sequelize
+const { Bancos } = require('../models'); // Sequelize
+const { Censos } = require('../models'); // Sequelize
+const { Denues } = require('../models'); // Sequelize
 
 const MODULE_NAME = '[GameSystem Controller]';
 // get estados
@@ -68,10 +71,171 @@ function getMunicipios(req, res) {
      }
     
 }
+
+function getBancos(req, res) {
+  try {
+        
+    //var params = {
+    //  entidad:   req.swagger.params.entidad.value
+    //};
+
+    console.log("Bancos...");
+    console.log(Bancos);
+    Bancos.findAll(
+      {
+        /*include: [{
+          model: Internets,
+          nested: false,
+          as: 'Internets',
+          attributes: ['habitadas'],
+        }]
+       // include: [{ all: true, nested: true }]
+        , */
+        //  where: {
+        //    idestado : params.entidad
+        //  }
+          //attributes: ['cve_mun', 'municipio'],
+      })
+    .then((bancos) => {
+      console.log(bancos);
+      res.status(200).send(bancos);
+      //utils.writeJson(res, consoles);
+    }, (error) => {
+      console.log("error : " + error);
+      res.status(500).send(error);
+    });
+ 
+   } catch (error) {
+     controllerHelper.handleErrorResponse(MODULE_NAME, getBancos.name, error, res);
+   }
+  
+}
+
+
+function getCensosByMun(req, res) {
+  try {
+        
+    var params = {
+      entidad:   req.swagger.params.entidad.value,
+      municipio: req.swagger.params.municipio.value
+    };
+
+    console.log("censos..." + params);
+    console.log(Censos);
+    Censos.findAll({
+     /*include: [{
+       model: orderstatus
+      
+     }]
+ 
+     include: [{ all: true, nested: true }]*/
+     where: {
+      idestado : params.entidad,
+      idmunicipio : params.municipio,
+      
+    }
+       })
+    .then((censos) => {
+      //console.log(consoles);
+      res.status(200).send(censos);
+      //utils.writeJson(res, consoles);
+    }, (error) => {
+      console.log("error : " + error);
+      res.status(500).send(error);
+    });
+ 
+   } catch (error) {
+     controllerHelper.handleErrorResponse(MODULE_NAME, getCensosByMun.name, error, res);
+   }
+  
+}
+
+
+
+function getCensosByEdo(req, res) {
+  try {
+        
+    var params = {
+      entidad:   req.swagger.params.entidad.value
+    };
+
+    console.log("censos..." + params);
+    console.log(Censos);
+    Censos.findAll({
+     /*include: [{
+       model: orderstatus
+      
+     }]
+ 
+     include: [{ all: true, nested: true }]*/
+     where: {
+      idestado : params.entidad,
+      actividad: 'Total municipal'
+
+    }
+       })
+    .then((censos) => {
+      //console.log(consoles);
+      res.status(200).send(censos);
+      //utils.writeJson(res, consoles);
+    }, (error) => {
+      console.log("error : " + error);
+      res.status(500).send(error);
+    });
+ 
+   } catch (error) {
+     controllerHelper.handleErrorResponse(MODULE_NAME, getCensosByEdo.name, error, res);
+   }
+  
+}
+
+
+function getDenues(req, res) {
+  try {
+        
+    var params = {
+      entidad:   req.swagger.params.entidad.value,
+      municipio: req.swagger.params.municipio.value
+    };
+
+    console.log("Denues..." + params);
+    console.log(Denues);
+    Denues.findAll({
+     /*include: [{
+       model: orderstatus
+      
+     }]
+ 
+     include: [{ all: true, nested: true }]*/
+     where: {
+      cve_ent : params.entidad,
+      cve_mun : params.municipio
+    }
+       })
+    .then((consoles) => {
+      //console.log(consoles);
+      res.status(200).send(consoles);
+      //utils.writeJson(res, consoles);
+    }, (error) => {
+      console.log("error : " + error);
+      res.status(500).send(error);
+    });
+ 
+   } catch (error) {
+     controllerHelper.handleErrorResponse(MODULE_NAME, getGameSystems.name, error, res);
+   }
+  
+
+}
+
 module.exports = {
     getMunicipios,
     getEstados,
-   // GS_CT_ERR_GAMESYSTEM_NOT_FOUND,
+    getBancos,
+    getCensosByMun,
+    getCensosByEdo,
+
+    // GS_CT_ERR_GAMESYSTEM_NOT_FOUND,
    // GS_CT_DELETED_SUCCESSFULLY,
     MODULE_NAME
   }
